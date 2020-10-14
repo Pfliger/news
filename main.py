70e6ae3359d08b385c1c85d8c89f7bf21c44b7f2
Pfliger <pfliger@list.ru>
2020-10-14 19:18:07 +0300

    modifi main

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import json
 import xml.etree.ElementTree as ET
+import collections
 
 
 def top_words(words, word_quantity=10):
-    number_of_reqest = {}
-    result = {}
-    quantity = 0
-    for word in words:
-        number_of_reqest[word] = number_of_reqest.setdefault(word, 0) + 1
-    сharacters_list = list(set(sorted(number_of_reqest.values())))
-    сharacters_list.reverse()
-    for item in сharacters_list[:word_quantity]:
-        for x, y in number_of_reqest.items():
-            if item == y and quantity < word_quantity:
-                result[x] = y
-                quantity += 1
+
+    result = collections.Counter(words).most_common(word_quantity)
+
+    # number_of_reqest = {}
+    # result2 = {}
+    # quantity = 0
+    # for word in words:
+    #     number_of_reqest[word] = number_of_reqest.setdefault(word, 0) + 1
+    # сharacters_list = list(set(sorted(number_of_reqest.values())))
+    # сharacters_list.reverse()
+    # for item in сharacters_list[:word_quantity]:
+    #     for x, y in number_of_reqest.items():
+    #         if item == y and quantity < word_quantity:
+    #             result2[x] = y
+    #             quantity += 1
     return result
 
 
@@ -24,11 +28,11 @@ def top_json(filename, word_quantity=10, min_len=6):
     with open(filename, encoding='utf-8') as file:
         file = json.load(file)
         news = file['rss']['channel']['items']
-        for item in news:
-            text += item['description'].split()
-        for item in text:
-            if len(item) > min_len:
-                result.append(item)
+    for item in news:
+        text += item['description'].split()
+    for item in text:
+        if len(item) > min_len:
+            result.append(item)
     return top_words(result, word_quantity)
 
 
